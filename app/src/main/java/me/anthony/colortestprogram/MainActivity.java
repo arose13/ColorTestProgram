ed4fefdf64f0edf48729dbf5bc81937e7787980e
arose13 <anthony_rose12@hotmail.com>
2015-05-28 11:04:42 -0400

    Initial commit

---
+++ app/src/main/java/me/anthony/colortestprogram/MainActivity.java
@@ -0,0 +1,71 @@
+package me.anthony.colortestprogram;
+
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private View mView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate() ran");
+
+        mView = findViewById(R.id.animateView);
+        mView.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.animateView:
+                Log.d(TAG, "White clicked");
+                // Animator a Value
+                ValueAnimator animator = ValueAnimator.ofInt(65, 25);
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        int value = (int) animation.getAnimatedValue();
+                        mView.setBackgroundColor(Color.GRAY);
+                    }
+                });
+                animator.setDuration(1150);
+                animator.start();
+                break;
+
+            default:
+                break;
+        }
+    }
+}
